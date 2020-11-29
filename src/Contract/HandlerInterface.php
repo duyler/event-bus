229203dff56b47bc44b136bc526a2a24119c466c
Mikhail Ilyinsky <ilinskiy_ma@drom.ru>
2020-11-29 22:44:52 +1000

    fix for php 8

--- src/Contract/HandlerInterface.php
+++ src/Contract/HandlerInterface.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace Jine\EventBus\Contract;
 
 use Jine\EventBus\Dto\Result;
-use Jine\Contracts\Service\ActionInterface;
 
 interface HandlerInterface
 {
-    public function run(ActionInterface $action): Result;
+    public function run(object $action): Result;
 
     public function getClass(): string;
 
