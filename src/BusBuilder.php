f34962c5548c24a3c47fd25128fc88d24b1fd473
Mikhail Ilinsky <mail@milinsky.ru>
2023-06-16 01:27:21 +1000

    Add shared services + refactoring

--- src/BusBuilder.php
+++ src/BusBuilder.php
@@ -20,6 +20,8 @@ use Duyler\EventBus\Dto\Subscription;
 use Duyler\EventBus\Service\ActionService;
 use Duyler\EventBus\Service\StateService;
 use Duyler\EventBus\Service\SubscriptionService;
+use Duyler\EventBus\State\StateHandlerInterface;
+use InvalidArgumentException;
 
 class BusBuilder
 {
@@ -34,28 +36,10 @@ class BusBuilder
     /** @var Action[] $doActions */
     private array $doActions = [];
 
-    /** @var StateMainStartHandlerInterface[] $stateMainStartHandlers */
-    private array $stateMainStartHandlers = [];
+    /** @var StateHandlerInterface[] $stateHandlers */
+    private array $stateHandlers = [];
 
-    /** @var StateMainBeforeHandlerInterface[] $stateMainBeforeHandlers */
-    private array $stateMainBeforeHandlers = [];
-
-    private StateMainSuspendHandlerInterface|null $stateMainSuspendHandler = null;
-
-    /** @var StateMainAfterHandlerInterface[] $stateMainAfterHandlers */
-    private array $stateMainAfterHandlers = [];
-
-    /** @var StateMainFinalHandlerInterface[] $stateMainFinalHandlers */
-    private array $stateMainFinalHandlers = [];
-
-    /** @var StateActionBeforeHandlerInterface[] $stateActionBeforeHandlers */
-    private array $stateActionBeforeHandlers = [];
-
-    /** @var StateActionThrowingHandlerInterface[] $stateActionThrowingHandlers */
-    private array $stateActionThrowingHandlers = [];
-
-    /** @var StateActionAfterHandlerInterface[] $stateActionAfterHandlers */
-    private array $stateActionAfterHandlers = [];
+    private array $sharedServices = [];
 
     private ?Config $config = null;
 
@@ -86,7 +70,10 @@ class BusBuilder
         $stateService = $container->make(StateService::class);
 
         $actionService->collect($this->actions);
-        $actionService->collect($this->doActions);
+
+        foreach ($this->sharedServices as $sharedService) {
+            $actionService->addSharedService($sharedService);
+        }
 
         foreach ($this->doActions as $action) {
             $actionService->doExistsAction($action->id);
@@ -96,36 +83,31 @@ class BusBuilder
             $subscriptionService->addSubscription($subscription);
         }
 
-        foreach ($this->stateMainStartHandlers as $handler) {
-            $stateService->addStateMainStartHandler($handler);
-        }
+        foreach ($this->stateHandlers as $stateHandler) {
+            match (true) {
+                $stateHandler instanceof StateMainStartHandlerInterface =>
+                $stateService->addStateMainStartHandler($stateHandler),
+                $stateHandler instanceof StateMainBeforeHandlerInterface =>
+                $stateService->addStateMainBeforeHandler($stateHandler),
+                $stateHandler instanceof StateMainSuspendHandlerInterface =>
+                $stateService->setStateMainSuspendHandler($stateHandler),
+                $stateHandler instanceof StateMainAfterHandlerInterface =>
+                $stateService->addStateMainAfterHandler($stateHandler),
+                $stateHandler instanceof StateMainFinalHandlerInterface =>
+                $stateService->addStateMainFinalHandler($stateHandler),
+                $stateHandler instanceof StateActionBeforeHandlerInterface =>
+                $stateService->addStateActionBeforeHandler($stateHandler),
+                $stateHandler instanceof StateActionThrowingHandlerInterface =>
+                $stateService->addStateActionThrowingHandler($stateHandler),
+                $stateHandler instanceof StateActionAfterHandlerInterface =>
+                $stateService->addStateActionAfterHandler($stateHandler),
 
-        foreach ($this->stateMainBeforeHandlers as $handler) {
-            $stateService->addStateMainBeforeHandler($handler);
-        }
-
-        if ($this->stateMainSuspendHandler !== null) {
-            $stateService->setStateMainSuspendHandler($this->stateMainSuspendHandler);
-        }
-
-        foreach ($this->stateMainAfterHandlers as $handler) {
-            $stateService->addStateMainAfterHandler($handler);
-        }
-
-        foreach ($this->stateMainFinalHandlers as $handler) {
-            $stateService->addStateMainFinalHandler($handler);
-        }
-
-        foreach ($this->stateActionBeforeHandlers as $handler) {
-            $stateService->addStateActionBeforeHandler($handler);
-        }
-
-        foreach ($this->stateActionThrowingHandlers as $handler) {
-            $stateService->addStateActionThrowingHandler($handler);
-        }
-
-        foreach ($this->stateActionAfterHandlers as $handler) {
-            $stateService->addStateActionAfterHandler($handler);
+                default => throw new InvalidArgumentException(sprintf(
+                    'State handler %s must be compatibility with %s',
+                    get_class($stateHandler),
+                    StateHandlerInterface::class,
+                ))
+            };
         }
 
         /** @var Runner $runner */
@@ -154,55 +136,20 @@ class BusBuilder
 
     public function doAction(Action $action): static
     {
+        $this->actions[$action->id] = $action;
         $this->doActions[$action->id] = $action;
         return $this;
     }
 
-    public function addStateMainStartHandler(StateMainStartHandlerInterface $startHandler): static
+    public function addStateHandler(StateHandlerInterface $stateHandler): static
     {
-        $this->stateMainStartHandlers[] = $startHandler;
+        $this->stateHandlers[] = $stateHandler;
         return $this;
     }
 
-    public function addStateMainBeforeHandler(StateMainBeforeHandlerInterface $beforeActionHandler): static
+    public function addSharedService(object $service): static
     {
-        $this->stateMainBeforeHandlers[] = $beforeActionHandler;
-        return $this;
-    }
-
-    public function setStateMainSuspendHandler(StateMainSuspendHandlerInterface $suspendHandler): static
-    {
-        $this->stateMainSuspendHandler = $suspendHandler;
-        return $this;
-    }
-
-    public function addStateMainAfterHandler(StateMainAfterHandlerInterface $afterActionHandler): static
-    {
-        $this->stateMainAfterHandlers[] = $afterActionHandler;
-        return $this;
-    }
-
-    public function addStateMainFinalHandler(StateMainFinalHandlerInterface $finalHandler): static
-    {
-        $this->stateMainFinalHandlers[] = $finalHandler;
-        return $this;
-    }
-
-    public function addStateActionBeforeHandler(StateActionBeforeHandlerInterface $actionBeforeHandler): static
-    {
-        $this->stateActionBeforeHandlers[] = $actionBeforeHandler;
-        return $this;
-    }
-
-    public function addStateActionThrowingHandler(StateActionThrowingHandlerInterface $actionThrowingHandler): static
-    {
-        $this->stateActionThrowingHandlers[] = $actionThrowingHandler;
-        return $this;
-    }
-
-    public function addStateActionAfterHandler(StateActionAfterHandlerInterface $actionAfterHandler): static
-    {
-        $this->stateActionAfterHandlers[] = $actionAfterHandler;
+        $this->sharedServices[] = $service;
         return $this;
     }
 }
