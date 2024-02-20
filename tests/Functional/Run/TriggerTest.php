0fb313f23d7e0a29ae663b2b334a0e027d3d9b29
Mikhail Ilinsky <mail@milinsky.ru>
2024-02-21 00:38:17 +1000

    Add trigger tests

---
+++ tests/Functional/Run/TriggerTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duyler\EventBus\Test\Functional\Run;
+
+use Duyler\EventBus\BusBuilder;
+use Duyler\EventBus\BusConfig;
+use Duyler\EventBus\Dto\Action;
+use Duyler\EventBus\Dto\Subscription;
+use Duyler\EventBus\Dto\Trigger;
+use Duyler\EventBus\Enum\ResultStatus;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class TriggerTest extends TestCase
+{
+    #[Test]
+    public function run_without_contract(): void
+    {
+        $builder = new BusBuilder(new BusConfig());
+        $builder->addAction(
+            new Action(
+                id: 'ForTriggerAction',
+                handler: function () {},
+                externalAccess: true,
+            )
+        );
+
+        $builder->addSubscription(
+            new Subscription(
+                subjectId: 'TestTrigger',
+                actionId: 'ForTriggerAction',
+            )
+        );
+
+        $bus = $builder->build();
+        $bus->dispatchTrigger(
+            new Trigger(
+                id: 'TestTrigger',
+                status: ResultStatus::Success,
+            )
+        );
+
+        $bus->run();
+
+        $this->assertTrue($bus->resultIsExists('ForTriggerAction'));
+        $this->assertTrue($bus->resultIsExists('TestTrigger'));
+    }
+
+    #[Test]
+    public function run_with_contract(): void
+    {
+        $builder = new BusBuilder(new BusConfig());
+        $builder->addAction(
+            new Action(
+                id: 'ForTriggerAction',
+                handler: function (stdClass $data) {},
+                argument: stdClass::class,
+                externalAccess: true
+            )
+        );
+
+        $builder->addSubscription(
+            new Subscription(
+                subjectId: 'TestTrigger',
+                actionId: 'ForTriggerAction',
+            )
+        );
+
+        $bus = $builder->build();
+        $bus->dispatchTrigger(
+            new Trigger(
+                id: 'TestTrigger',
+                status: ResultStatus::Success,
+                data: new stdClass(),
+                contract: stdClass::class,
+            )
+        );
+
+        $bus->run();
+
+        $this->assertTrue($bus->resultIsExists('ForTriggerAction'));
+        $this->assertTrue($bus->resultIsExists('TestTrigger'));
+        $this->assertInstanceOf(stdClass::class, $bus->getResult('TestTrigger')->data);
+    }
+}
