420b9f0a67d231c02407466661d07f71f181f0fa
Mikhail Ilinsky <mail@milinsky.ru>
2024-02-20 17:33:12 +1000

    Fix Action argument type

--- src/Action/ActionHandlerArgumentBuilder.php
+++ src/Action/ActionHandlerArgumentBuilder.php
@@ -25,7 +25,7 @@ class ActionHandlerArgumentBuilder
     /** @psalm-suppress MixedReturnStatement */
     public function build(Action $action, ActionContainer $container): null|object
     {
-        if (empty($action->argument)) {
+        if ($action->argument === null) {
             return null;
         }
 
