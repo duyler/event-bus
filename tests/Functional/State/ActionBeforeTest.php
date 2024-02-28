0abf3a4adbf5161577eb91ce02fada0bb3330dbb
Mikhail Ilinsky <mail@milinsky.ru>
2024-02-28 23:19:42 +1000

    Add undefinded subscription test

--- tests/Functional/State/ActionBeforeTest.php
+++ tests/Functional/State/ActionBeforeTest.php
@@ -9,6 +9,8 @@ use Duyler\EventBus\BusConfig;
 use Duyler\EventBus\Contract\State\ActionBeforeStateHandlerInterface;
 use Duyler\EventBus\Dto\Action;
 use Duyler\EventBus\Dto\Context;
+use Duyler\EventBus\Dto\Subscription;
+use Duyler\EventBus\Exception\SubscriptionNotFoundException;
 use Duyler\EventBus\State\Service\StateActionBeforeService;
 use Duyler\EventBus\State\StateContext;
 use Override;
@@ -36,6 +38,24 @@ class ActionBeforeTest extends TestCase
         $bus = $busBuilder->build()->run();
         $this->assertTrue($bus->resultIsExists('Test'));
     }
+
+    #[Test]
+    public function before_with_remove_not_found_subscription(): void
+    {
+        $busBuilder = new BusBuilder(new BusConfig());
+        $busBuilder->addStateHandler(new ActionBeforeThrowsStateHandler());
+        $busBuilder->doAction(
+            new Action(
+                id: 'ExistsAction',
+                handler: function () {},
+                externalAccess: true,
+            )
+        );
+
+        $bus = $busBuilder->build();
+        $this->expectException(SubscriptionNotFoundException::class);
+        $bus->run();
+    }
 }
 
 class ActionBeforeStateHandler implements ActionBeforeStateHandlerInterface
@@ -45,6 +65,24 @@ class ActionBeforeStateHandler implements ActionBeforeStateHandlerInterface
     {
         $stateService->getContainer();
         $stateService->getActionId();
+    }
+
+    #[Override]
+    public function observed(StateContext $context): array
+    {
+        return [];
+    }
+}
+
+class ActionBeforeThrowsStateHandler implements ActionBeforeStateHandlerInterface
+{
+    #[Override]
+    public function handle(StateActionBeforeService $stateService, StateContext $context): void
+    {
+        $stateService->removeSubscription(new Subscription(
+            subjectId: 'TestNotExists',
+            actionId: 'Test',
+        ));
     }
 
     #[Override]
