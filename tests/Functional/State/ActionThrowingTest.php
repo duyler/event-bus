bc8e913ec405b4a4a98ece47d892be9ee5938dad
Mikhail Ilinsky <mail@milinsky.ru>
2024-02-28 22:24:16 +1000

    Add action throwing test

---
+++ tests/Functional/State/ActionThrowingTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duyler\EventBus\Test\Functional\State;
+
+use Duyler\EventBus\BusBuilder;
+use Duyler\EventBus\BusConfig;
+use Duyler\EventBus\Contract\State\ActionThrowingStateHandlerInterface;
+use Duyler\EventBus\Dto\Action;
+use Duyler\EventBus\State\Service\StateActionThrowingService;
+use Duyler\EventBus\State\StateContext;
+use Override;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+class ActionThrowingTest extends TestCase
+{
+    #[Test]
+    public function throw_without_rollback(): void
+    {
+        $busBuilder = new BusBuilder(new BusConfig());
+        $busBuilder->addStateHandler(new ActionThrowingStateHandler());
+        $busBuilder->doAction(
+            new Action(
+                id: 'Test',
+                handler: function () {throw new RuntimeException('Test exception message');},
+                externalAccess: true,
+            )
+        );
+
+        $busBuilder->build();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Test exception message');
+        $busBuilder->build()->run();
+    }
+}
+
+class ActionThrowingStateHandler implements ActionThrowingStateHandlerInterface
+{
+    #[Override]
+    public function handle(StateActionThrowingService $stateService, StateContext $context): void
+    {
+        $stateService->getException();
+        $stateService->getActionId();
+        $stateService->getContainer();
+    }
+
+    #[Override] public function observed(StateContext $context): array
+    {
+        return [];
+    }
+}
