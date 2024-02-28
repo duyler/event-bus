70dbc47e255975b86ad9797a34bc56938825fa91
Mikhail Ilinsky <mail@milinsky.ru>
2024-02-28 23:38:35 +1000

    Add build with shared service test

---
+++ tests/Functional/Build/AddSharedServiceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Duyler\EventBus\Test\Functional\Build;
+
+use Duyler\EventBus\BusBuilder;
+use Duyler\EventBus\BusConfig;
+use Duyler\EventBus\Dto\Action;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class AddSharedServiceTest extends TestCase
+{
+    #[Test]
+    public function addSharedService_with_bind()
+    {
+        $busBuilder = new BusBuilder(new BusConfig());
+        $busBuilder->addSharedService(
+            new SharedService('Test service'),
+            [
+                SharedInterface::class => SharedService::class,
+            ]
+        );
+
+        $busBuilder->doAction(
+            new Action(
+                id: 'Test',
+                handler: Handler::class,
+                contract: SharedInterface::class,
+                externalAccess: true
+            )
+        );
+
+        $bus = $busBuilder->build()->run();
+
+        $this->assertTrue($bus->resultIsExists('Test'));
+        $this->assertEquals('Test service', $bus->getResult('Test')->data->foo);
+    }
+}
+
+readonly class SharedService implements SharedInterface
+{
+    public function __construct(public string $foo) {}
+}
+
+interface SharedInterface {}
+
+readonly class Handler
+{
+    public function __construct(
+        public SharedInterface $shared
+    ) {}
+
+    public function __invoke(): SharedInterface
+    {
+        return $this->shared;
+    }
+}
