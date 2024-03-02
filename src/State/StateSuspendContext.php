cff267a5d3c07d1224fd9a26eadde73799739561
Mikhail Ilinsky <mail@milinsky.ru>
2024-03-02 18:27:04 +1000

    Fix action resuming value

--- src/State/StateSuspendContext.php
+++ src/State/StateSuspendContext.php
@@ -6,18 +6,16 @@ namespace Duyler\EventBus\State;
 
 class StateSuspendContext
 {
+    /** @var array<string, mixed[]> */
     private array $resumeValues = [];
 
     public function addResumeValue(string $actionId, mixed $value): void
     {
-        $this->resumeValues[$actionId] = $value;
+        $this->resumeValues[$actionId][] = $value;
     }
 
     public function getResumeValue(string $actionId): mixed
     {
-        $value = $this->resumeValues[$actionId];
-        unset($this->resumeValues[$actionId]);
-
-        return $value;
+        return array_shift($this->resumeValues[$actionId]);
     }
 }
