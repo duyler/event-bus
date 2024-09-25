8784b4a31224f3eb3c975755128171093f2abff4
Mikhail Ilinsky <milinsky@MacBook-Pro--Milinsky.local>
2024-09-25 16:43:12 +1000

    Fix action flush

--- src/Internal/Listener/Bus/FlushActionSuccessLogEventListener.php
+++ src/Internal/Listener/Bus/FlushActionSuccessLogEventListener.php
@@ -5,18 +5,23 @@ declare(strict_types=1);
 namespace Duyler\EventBus\Internal\Listener\Bus;
 
 use Duyler\EventBus\Bus\Log;
+use Duyler\EventBus\Enum\ResultStatus;
 use Duyler\EventBus\Internal\Event\TaskAfterRunEvent;
+use Duyler\EventBus\Storage\CompleteActionStorage;
 
 class FlushActionSuccessLogEventListener
 {
     public function __construct(
         private Log $log,
+        private CompleteActionStorage $completeActionStorage,
     ) {}
 
     public function __invoke(TaskAfterRunEvent $event): void
     {
         if ($event->task->action->flush) {
-            $this->log->flushSuccessLog();
+            if (ResultStatus::Success === $this->completeActionStorage->getResult($event->task->action->id)->status) {
+                $this->log->flushSuccessLog();
+            }
         }
     }
 }
