4f928204a18772090d9bbf3e84ebd0b0362af60a
Mikhail Ilinsky <mail@milinsky.ru>
2025-05-18 19:35:23 +1000

    Set listener type

--- src/Internal/EventDispatcher.php
+++ src/Internal/EventDispatcher.php
@@ -8,13 +8,14 @@ use Override;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 
-class EventDispatcher implements EventDispatcherInterface
+final class EventDispatcher implements EventDispatcherInterface
 {
     public function __construct(private ListenerProviderInterface $listenerProvider) {}
 
     #[Override]
     public function dispatch(object $event): void
     {
+        /** @var callable $listener */
         foreach ($this->listenerProvider->getListenersForEvent($event) as $listener) {
             $listener($event);
         }
