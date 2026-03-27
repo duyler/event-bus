ec967e43dd13d18a576fe1a39f08aedb4caa0a2f
Mikhail Ilinsky <mail@milinsky.ru>
2026-03-28 02:39:08 +1000

    fix: Add scope in Channel

--- src/Channel/Channel.php
+++ src/Channel/Channel.php
@@ -21,9 +21,8 @@ final class Channel implements ChannelInterface
         self::$transfer = $transfer;
     }
 
-    public static function open(
-        string $name = self::DEFAULT_CHANNEL,
-    ): ChannelInterface {
+    public static function open(string $name = self::DEFAULT_CHANNEL): ChannelInterface
+    {
         return new Channel(self::$transfer, $name);
     }
 
@@ -35,10 +34,10 @@ final class Channel implements ChannelInterface
         /**
          * @var Closure $callback
          */
-        $callback = Fiber::suspend(fn(): Closure => function (Transfer $transfer): mixed {
+        $callback = Fiber::suspend(fn(string $scope): Closure => function (Transfer $transfer) use ($scope): mixed {
             while ($transfer->isValid()) {
-                if ($transfer->has($this->name)) {
-                    return $transfer->get($this->name);
+                if ($transfer->has($this->name . '.' . $scope)) {
+                    return $transfer->get($this->name . '.' . $scope);
                 }
                 Fiber::suspend();
             }
@@ -51,7 +50,12 @@ final class Channel implements ChannelInterface
     #[Override]
     public function send(mixed $value): void
     {
-        self::$transfer->push($this->name, $value);
-        Fiber::suspend();
+        Fiber::suspend(function (string $scope) use ($value): void {
+            self::$transfer->push($this->name . '.' . $scope, $value);
+        });
+
+        if (self::$transfer->count() < 2) {
+            Fiber::suspend();
+        }
     }
 }
